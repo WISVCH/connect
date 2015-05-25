bbceb514ab03973f701c41dcc09df6f85018ea4e
Mark Janssen <mark@praseodym.net>
2015-05-25 16:35:16 +0200

    Show Person details on protected page

--- src/main/java/samltest/MainController.java
+++ src/main/java/samltest/MainController.java
@@ -1,15 +1,11 @@
 package samltest;
 
+import ch.wisv.dienst2.apiclient.model.Person;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.opensaml.saml2.core.Attribute;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.saml.SAMLCredential;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
@@ -21,13 +17,11 @@ public class MainController {
     @RequestMapping("/protected")
     @ResponseBody
     public String protectedpage(Authentication authentication) throws JsonProcessingException {
-        Object credentials = authentication.getCredentials();
-        if (credentials != null && credentials instanceof SAMLCredential) {
-            SAMLCredential samlCredential = (SAMLCredential) credentials;
-            List<Attribute> attributes = samlCredential.getAttributes();
-            String attributesString = attributes.stream().map(Attribute::getName).map(n -> n + ": " + samlCredential
-                    .getAttributeAsString(n)).collect(Collectors.joining(",\n"));
-            return "<pre>Authenticated\n\n" + authentication.getName() + "\n\n" + attributesString;
+        if (authentication instanceof CHAuthenticationToken) {
+            Person person = ((CHAuthenticationToken)
+                    authentication).getPerson();
+            return String.format("<pre>Authenticated\n\n%s\n\n%s %s %s", authentication.getName(), person
+                    .getFirstname(), person.getPreposition(), person.getSurname());
         } else {
             return "<pre>Authenticated\n\n" + authentication.getName();
         }
