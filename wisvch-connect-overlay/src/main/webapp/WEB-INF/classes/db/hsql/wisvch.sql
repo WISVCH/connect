f1a7cf1410baae24a8d4ceec9896113e2ca39c8c
Mark Janssen <mark@praseodym.net>
2018-01-20 19:04:53 +0100

    Add defaults for local testing

---
+++ wisvch-connect-overlay/src/main/webapp/WEB-INF/classes/db/hsql/wisvch.sql
@@ -0,0 +1,38 @@
+SET AUTOCOMMIT FALSE;
+START TRANSACTION;
+
+
+INSERT INTO client_redirect_uri_TEMP (owner_id, redirect_uri) VALUES
+  ('client', 'http://localhost:3000/oauth/callback');
+
+INSERT INTO system_scope (scope, description, icon, restricted, default_scope) VALUES
+  ('student', 'NetID and student number', 'book', FALSE, TRUE),
+  ('ldap', 'CH LDAP information', 'briefcase', FALSE, TRUE);
+
+
+MERGE INTO client_redirect_uri
+USING (SELECT
+         id,
+         redirect_uri
+       FROM client_redirect_uri_TEMP, client_details
+       WHERE client_details.client_id = client_redirect_uri_TEMP.owner_id) AS vals(id, redirect_uri)
+ON vals.id = client_redirect_uri.owner_id AND vals.redirect_uri = client_redirect_uri.redirect_uri
+WHEN NOT MATCHED THEN
+  INSERT (owner_id, redirect_uri) VALUES (vals.id, vals.redirect_uri);
+
+MERGE INTO system_scope
+USING (SELECT
+         scope,
+         description,
+         icon,
+         restricted,
+         default_scope
+       FROM system_scope_TEMP) AS vals(scope, description, icon, restricted, default_scope)
+ON vals.scope = system_scope.scope
+WHEN NOT MATCHED THEN
+  INSERT (scope, description, icon, restricted, default_scope)
+  VALUES (vals.scope, vals.description, vals.icon, vals.restricted, vals.default_scope);
+
+
+COMMIT;
+SET AUTOCOMMIT TRUE;
